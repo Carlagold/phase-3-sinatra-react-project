af61e3c00149ee237946da00dcc38441590377c3
Carla <csachett@student.scf.edu>
2022-10-25 15:40:17 -0400

    Controller/ user router-create-get/ new user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,27 @@ class ApplicationController < Sinatra::Base
   
   # Add your routes here
   get "/" do
-    { message: "Good luck with your project!" }.to_json
+    { message: "I hope this works...!" }.to_json
+  end
+  # create a new user
+  get "/user" do
+    a_user = User.all
+    a_user.to_json
+  end
+  # get a specific user
+  get "/user/:user_id" do
+    u_user = User.find(params[:user_id])
+    u_user.to_json
   end
 
+  # Create new user
+  post "/user/:id" do
+    new_user = User.create(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      username: params[:username],
+      password: params[:password]
+    )
+    new_user.to_json
+  end
 end
